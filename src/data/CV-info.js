d29dbbb8ac0d73033e249fcdabfdc69afec757c7
João Afonso <joaoafonso386@gmail.com>
2021-10-30 18:54:10 +0100

    Adding all the information for each component

--- src/data/CV-info.js
+++ src/data/CV-info.js
@@ -12,7 +12,58 @@ export const CV = {
     content:
       "I studied marketing and in 2020 started to develop interest in web development. I found this passion that is coding and fell in love with the tech industry. Currently working with front-end techonolgies but my intention is to be a complete developer, being able to create an application from start to finish.",
   },
-  skills: [],
-  education: [],
-  experience: [],
+  skills: [
+    {
+      title: "Web Design",
+      content: "Working on developing layouts for websites",
+    },
+    {
+      title: "Responsive",
+      content: "Making layouts adapt to any screen size",
+    },
+    {
+      title: "Development",
+      content: "Front to back application building and testing",
+    },
+    {
+      title: "SEO Friendly",
+      content: "Working with search engines to optimize performance",
+    },
+    {
+      title: "Graphics Design",
+      content: "Creating pixel perfect prototypes to implement",
+    },
+    {
+      title: "Support",
+      content: "Able to work in teams to better development environment",
+    },
+  ],
+  education: [
+    {
+      institution: "ULHT - Universidade Lusófona",
+      degree: "Bachelors in Marketing and PR",
+      description:
+        "First steps as a marketer, helped me choose what I was really interested about",
+    },
+    {
+      institution: "NOVA IMS",
+      degree: "Post-grad in Digital Marketing",
+      description:
+        "Specific knowledge in digital marketing and digital marketing tools",
+    },
+  ],
+  experience: [
+    {
+      company: "Youzz",
+      jobCategory: "Marketing Influencing",
+      description:
+        "Developed contacts with influencers and helped create a strong social media presence",
+    },
+    {
+      company: "Caiado Guerreio",
+      jobCategory: "Digital Marketing",
+      description:
+        "Worked as media planner and helped the company to have better visibility online",
+    },
+  ],
 };
